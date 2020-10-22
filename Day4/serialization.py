bfa93ab21ff28beca70512164a1194868efe6e22
nelsonomoi <omoinelson3@gmail.com>
2020-10-22 21:06:20 +0300

    json serilization

--- Day4/serialization.py
+++ Day4/serialization.py
@@ -2,5 +2,12 @@
 
 import simplejson as json
 
+# dump() will write Python data to a file-like object. We use this when we want to
+# serialize our Python data to an external JSON file.
+#
+# dumps() will write Python data to a string in JSON format. This is useful if we want
+# to use the JSON elsewhere in our program, or if we
+# just want to print it to the console to check that it’s correct.
+
 json_string = json.dumps([1, 2, 3, "a", "b", "c"])
 print(json_string)
